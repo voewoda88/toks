d85d710a8404827b1ff2530595bdf945a8e76890
voewoda88 <voewoda88@gmail.com>
2023-09-24 15:45:32 +0300

    init: added global variables

---
+++ lab1/globalVariables.py
@@ -0,0 +1,10 @@
+serialPortSender1 = '/dev/tnt0'
+serialPortReciever1 = '/dev/tnt1'
+serialPortSender2 = '/dev/tnt2'
+serialPortReciever2 = '/dev/tnt3'
+serialPortSender3 = '/dev/tnt4'
+serialPortReciever3 = '/dev/tnt5'
+serialPortSender4 = '/dev/tnt6'
+serialPortReciever4 = '/dev/tnt7'
+
+parity = None
