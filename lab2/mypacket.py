c65cc10f8249b134ad5279700cfd27a3de39b159
voewoda88 <voewoda88@gmail.com>
2023-09-30 17:00:17 +0300

    feat: added class for packet

---
+++ lab2/mypacket.py
@@ -0,0 +1,47 @@
+class MyPacket:
+    def __init__(self):
+        self.flag = 0
+        self.destinationAddress = 0
+        self.sourceAddress = 0
+        self.data = ''
+        self.fcs = 0
+
+    def packageFormation(self, sourceAddress, data):
+        self.flag = ord('z') + 1  # Флаг (1 байт)
+        self.destinationAddress = 0  # Destination Address (1 байт)
+        self.sourceAddress = sourceAddress  # Source Address (1 байт, номер ком-порта)
+        self.data = data  # Data (1 байт, данные)
+        self.dataBytes = self.data.encode('utf-8')
+        self.fcs = 0  # FCS (1 байт, всегда нулевой)
+
+    def byteStuffing(self):
+        data = bytes([self.flag, self.destinationAddress, self.sourceAddress]) + self.dataBytes + bytes([self.fcs])
+        stuffedData = []
+        for byte in data:
+            if byte == 0x7E:
+                stuffedData.extend([0x7D, 0x5E])
+            elif byte == 0x7D:
+                stuffedData.extend([0x7D, 0x5D])
+            else:
+                stuffedData.append(byte)
+        return bytes(stuffedData)
+
+    @staticmethod
+    def byteDestuffing(data):
+        destuffedData = []
+        i = 0
+        while i < len(data):
+            if data[i] == 0x7D:
+                if data[i + 1] == 0x5E:
+                    destuffedData.append(0x7E)
+                    i += 2
+                elif data[i + 1] == 0x5D:
+                    destuffedData.append(0x7D)
+                    i += 2
+                else:
+                    # Неверная последовательность символов байт-стаффинга, обработать по необходимости
+                    i += 1
+            else:
+                destuffedData.append(data[i])
+                i += 1
+        return destuffedData
